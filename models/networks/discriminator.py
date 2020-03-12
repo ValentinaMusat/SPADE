ca17edcfa2d61dab8b9812beb3c86006637ba8fa
Valentina Musat <you@example.com>
2020-03-12 03:34:12 +0000

    secondary dataset 2

--- models/networks/discriminator.py
+++ models/networks/discriminator.py
@@ -105,6 +105,8 @@ class NLayerDiscriminator(BaseNetwork):
             input_nc += 1
         if not opt.no_instance:
             input_nc += 1
+        if opt.dataset_mode_secondary != "":
+            input_nc += 1
         return input_nc
 
     def forward(self, input):
